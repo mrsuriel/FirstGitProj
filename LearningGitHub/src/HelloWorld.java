bd09c8c32ac3f4ab34e3fcc30d6e53a347bd143e
Emmanuel Suriel <emsuriel@gmail.com>
2017-05-01 19:07:23 -0400

    second attempt

--- LearningGitHub/src/HelloWorld.java
+++ LearningGitHub/src/HelloWorld.java
@@ -3,6 +3,8 @@ public class HelloWorld {
 	public static void main(String[] args)
 	{
 		//this is an edit from GitHub!!!
+		
+		//this is a second edit from comp
 		System.out.println("hello world");
 	}
 }
